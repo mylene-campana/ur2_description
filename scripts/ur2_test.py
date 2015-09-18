3b07a6507157a4acc604ad3434a031c980183552
Mylene Campana <mylene.campana@laas.fr>
2015-09-18 15:37:31 +0200

    Comment second cylinder, enhance script

--- scripts/ur2_test.py
+++ scripts/ur2_test.py
@@ -25,8 +25,8 @@ r.loadObstacleModel ("ur2_description","cylinder_obstacle","cylinder_obstacle")
 # q = [x, y] # limits in URDF file
 #q1 = [-0.2, 1.6, -0.4]; q2 = [-0.2, 0.4, 0.5]
 q1 = [-0.2, 1.6, -0.4, -1.35, -0.1];
-q2 = [-0.2, 0.4, 0.5, -1.2, -0.4] # with 2 other cylinders
-#q2 = [-0.2, 0.4, 0.5, -1.35, -0.1]; # with one cylinder
+#q2 = [-0.2, 0.4, 0.5, -1.2, -0.4] # with 2 other cylinders
+q2 = [-0.2, 0.4, 0.5, -1.35, -0.1]; # with one cylinder
 r(q1)
 #robot.isConfigValid(q1); robot.isConfigValid(q2) 
 
@@ -34,19 +34,23 @@ cl.problem.setInitialConfig (q1); cl.problem.addGoalConfig (q2)
 
 # config inversion ??? viewer and hpp don't read joint in same order ??
 #ps.selectPathPlanner ("VisibilityPrmPlanner") 
+#ps.selectPathValidation ("Dichotomy", 0.)
 ps.solve ()
 ps.pathLength(0)
 
+ps.addPathOptimizer('RandomShortcut')
+ps.optimizePath (0)
+ps.pathLength(1)
+
+ps.clearPathOptimizers()
 ps.addPathOptimizer("GradientBased")
 ps.optimizePath (0)
 ps.numberPaths()
 ps.pathLength(ps.numberPaths()-1)
 pp(ps.numberPaths()-1)
 
-ps.clearPathOptimizers()
-ps.addPathOptimizer('RandomShortcut')
-ps.optimizePath (0)
-ps.pathLength(1)
+
+
 
 
 for i in range(0,ps.numberPaths()):
@@ -54,8 +58,8 @@ for i in range(0,ps.numberPaths()):
 
 len(ps.getWaypoints (0))
 
+## Video recording
 pp.dt = 0.02
-
 r.startCapture ("capture","png")
 pp(ps.numberPaths()-1)
 r.stopCapture ()
@@ -64,6 +68,7 @@ r.stopCapture ()
 ffmpeg -r 50 -i capture_0_%d.png -r 25 -vcodec libx264 video.mp4
 x=0; for i in *png; do counter=$(printf %03d $x); ln "$i" new"$counter".png; x=$(($x+1)); done
 ffmpeg -r 50 -i new%03d.png -r 25 -vcodec libx264 video.mp4
+
 #####################################################################
 
 ## DEBUG commands
